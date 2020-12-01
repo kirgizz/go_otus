326d4e6ec4670947b43957f42fdb0085c47def2e
esergeev <esergeev@octafx.com>
2020-12-01 19:20:25 +0000

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	ntpServer := "0.beevik-ntp.pool.ntp.org"
+	serverTime := time.Now()
+	ntpTime, err := ntp.Time(ntpServer)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	fmt.Printf("current time: %s\n", serverTime.Round(0))
+	fmt.Printf("exact time: %s\n", ntpTime.Round(0))
 }
--- hw12_13_14_15_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_calendar/cmd/calendar/main.go
@@ -5,10 +5,10 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/app"
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/logger"
-	internalhttp "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/server/http"
-	memorystorage "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/storage/memory"
+	"github.com/kirgizz/hw12_13_14_15_calendar/internal/app"
+	"github.com/kirgizz/hw12_13_14_15_calendar/internal/logger"
+	internalhttp "github.com/kirgizz/hw12_13_14_15_calendar/internal/server/http"
+	memorystorage "github.com/kirgizz/hw12_13_14_15_calendar/internal/storage/memory"
 )
 
 var configFile string
